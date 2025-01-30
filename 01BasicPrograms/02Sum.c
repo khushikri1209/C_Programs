7d623b1307255e2232ffe8007b64a85976241e99
khushikri1209 <khushikumari12029@gmail.com>
2025-01-30 21:13:22 +0530

    Create 02Sum.c

---
+++ 01BasicPrograms/02Sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int num1, num2, sum;
+    printf("Enter first num:");
+    scanf("%d", &num1);
+    printf("Enter second num:");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("%d + %d=%d", num1, num2, sum);
+    getch ();
+
+}
