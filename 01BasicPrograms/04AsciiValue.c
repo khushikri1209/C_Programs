05d464247ff6d992236004369fceca9de73e8c39
khushikri1209 <khushikumari12029@gmail.com>
2025-01-31 10:22:05 +0530

    Create 04AsciiValue.c

---
+++ 01BasicPrograms/04AsciiValue.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    getch ();
+}
