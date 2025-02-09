d5ecd81b3d350204d31d245279da6b184be19b59
khushikri1209 <khushikumari12029@gmail.com>
2025-02-09 10:20:56 +0530

    Create 02HollowSquare.c

---
+++ 04patternPrograms/02HollowSquare.c
@@ -0,0 +1,31 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+     int i, j, N;
+ 
+     /* Input number of rows from user */
+     printf("Enter number of rows: ");
+     scanf("%d", &N);
+ 
+     /* Iterate over each row */
+     for(i=1; i<=N; i++)
+     {
+         /* Iterate over each column */
+         for(j=1; j<=N; j++)
+         {
+             if(i==1 || i==N || j==1 || j==N)
+             {
+                 /* Print star for 1st, Nth row and column */
+                 printf("*");
+             }
+             else
+             {
+                 printf(" ");
+             }
+         }
+         printf("\n");
+     }
+ 
+     getch();
+ }
