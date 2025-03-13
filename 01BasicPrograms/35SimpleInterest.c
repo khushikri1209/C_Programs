30978cb09c4c16a2b8db66194e7bf6356d1ee1b7
khushikri1209 <khushikumari12029@gmail.com>
2025-03-13 21:58:42 +0530

    Create 35SimpleInterest.c

---
+++ 01BasicPrograms/35SimpleInterest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    float principle, rate, time, SI, Amount;
+    printf("Enter Principle:- ");
+    scanf("%f", &principle);
+    printf("Enter Rate: ");
+    scanf("%f", &rate);
+    printf("Enter time: ");
+    scanf("%f", &time);
+}
